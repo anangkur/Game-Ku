c5be95bb6ff098cc557f5c3e2dbae702c3b51efa
anangkur <anangk97@gmail.com>
2023-10-31 10:42:41 +0800

    fix: remove comments

---
+++ lib/game.dart
@@ -0,0 +1,11 @@
+class Game {
+  int id;
+  String slug;
+  String name;
+  String backgroundImage;
+  double rating;
+  int ratingCount;
+
+  Game(this.id, this.slug, this.name, this.backgroundImage,
+      this.rating, this.ratingCount);
+}
